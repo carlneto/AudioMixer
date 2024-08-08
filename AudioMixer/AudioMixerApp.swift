0c6e61044cb5c316a868db1314b3a9ef0c76e35d
carlneto <carlneto@icloud.com>
2024-08-08 14:27:41 +0100

    Initial Commit

---
+++ AudioMixer/AudioMixerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioMixerApp.swift
+//  AudioMixer
+//
+//  Created by Carlos Neto on 08/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct AudioMixerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
